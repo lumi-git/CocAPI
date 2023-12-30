9798e75854eff76886f1fe03c7d62a97db28ab25
ronan tremoureux <ronan.tremoureux@gmail.com>
2023-12-30 11:57:32 +0100

    token doc

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 
-# Constants
+# Create a new file named TOKEN and add your token into it.
+# To get yourself a token, go at : https://developer.clashofclans.com/, register and create a token.
 API_TOKEN = open("TOKEN",'r').read()
 PLAYER_TAG = "#VPJY8CVG"
 API_URL = f"https://api.clashofclans.com/v1/players/%23{PLAYER_TAG.replace('#', '')}"
